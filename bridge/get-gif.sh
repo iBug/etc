047a738101b7fe57d385d7748210913698b70995
iBug <7273074+iBug@users.noreply.github.com>
2019-01-20 12:03:26 +0800

    de-HTML-ify

---
+++ bridge/get-gif.sh
@@ -0,0 +1,1 @@
+for i in sayc newsuitline top alogo2 c d s h; do wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' 'http://nsc.yam.org.tw/chi/htm/ebridge/sayc/'sayc.gif; done
